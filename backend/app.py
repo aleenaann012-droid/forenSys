96f428932611d5e9b209627f36e4eecb958fbeea
joelmathewgeorge <mathewjoel010@gmail.com>
2026-02-03 14:24:34 +0530

    modified

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from flask_cors import CORS
+from routes import api_bp
 
-from api.routes import api_bp
-
-app = Flask(__name__)
-CORS(app)
-
-app.register_blueprint(api_bp)
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(api_bp)
+    return app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app = create_app()
+    app.run(host="0.0.0.0", port=5000, debug=True)
